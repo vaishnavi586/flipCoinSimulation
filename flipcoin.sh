9c3a2defceb2da52ea33cb13389303ad851172a7
vaishnavi586 <machavolu1996@gmail.com>
2020-11-09 08:54:36 +0530

    displayed winner after flipping multiple times

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,11 +1,20 @@
 #!/bin/bash -x
+tails=0;
+heads=0;
+while [ $tails -lt 11 -a $heads -lt 11 ]
+do
 toss=$((RANDOM%2))
 if [ $toss -eq 0 ]
 then
- echo "tails is winner"
+    tails=$(($tails+1))
+    echo tails
 else
-   echo "heads is winner"
+    heads=$(($heads+1))
+    echo heads
 fi
+done
+echo "no of times head won:$heads"
+echo "no of times tails won: $tails"
 
 
 
